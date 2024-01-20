dc0bae0af85cf314a1dbfef800098edb96b3bbf6
Akeem Smith <asmith.339@outlook.com>
2024-01-20 11:04:26 -0400

    Fixed incorrect logic in script

--- data-clean/script.py
+++ data-clean/script.py
@@ -1,7 +1,10 @@
 import pandas as p
 import json
 
-exam_data = p.read_csv("./data/examlist.csv")
+"""
+PYTHON SCRIPT TO CLEAN DATA IN DATA-INITIAL FOLDER
+"""
+exam_data = p.read_csv("./data-initial/examlist.csv")
 
 # Removing blank values 
 exam_data.dropna(inplace=True)
@@ -10,15 +13,15 @@ exam_data.dropna(inplace=True)
 exam_data['time'] = exam_data['time'].astype(str)
 
 exam_data.loc[exam_data['time'] == '1:00 am','time'] = '1:00 pm'
-exam_data.loc[exam_data['time'] == '4:00 am','time'] = '2:00 pm'
-exam_data.loc[exam_data['time'] == '2:00 am','time'] = '4:00 pm'
+exam_data.loc[exam_data['time'] == '2:00 am','time'] = '2:00 pm'
+exam_data.loc[exam_data['time'] == '4:00 am','time'] = '4:00 pm'
 
-# chaning HRS to hours
+# changing HRS to hours
 exam_data["hours"] = exam_data["hours"].str.replace('HRS', 'hours')
     
 
 # Appending cleaned file to examlist_clean.csv
 exam_data.to_csv('./data-clean/examlist_clean.csv', mode='a', index=False)
 
-# Converting cleaned file to json
+# Converting cleaned file to json and storing in it examlist_clean.json
 json.dumps(json.loads(exam_data.to_json('./data-clean/examlist_clean.json', orient='records')))
