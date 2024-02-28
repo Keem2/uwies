8203e79cb45e23f03448f723ec27af7ccecc934a
Akeem Smith <asmith.339@outlook.com>
2024-02-28 07:49:11 -0400

    Delete frontend/src/utils/supbaseClient.js

--- frontend/src/utils/supbaseClient.js
+++
@@ -1,9 +0,0 @@
-import { createClient } from "@supabase/supabase-js";
-
-//supabase connection
-const supabase = createClient(
-   import.meta.env.VITE_SUPABASE_UWIES_URL,
-   import.meta.env.VITE_SUPABASE_UWIES_ANON_PUBLIC
-);
-
-export default supabase;
