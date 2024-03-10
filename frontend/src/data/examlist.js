a52036328ee12ba6ee9168cd3afdd4d047086f68
Akeem Smith <asmith.339@outlook.com>
2024-03-10 16:57:45 -0400

    changed function to sort examlist

--- frontend/src/data/examlist.js
+++ frontend/src/data/examlist.js
@@ -1,6 +1,17 @@
 import examlist from "./examlist_clean.json" assert { type: "json" };
 
 //Have Node 21+ for this to work
-const result = Object.groupBy(examlist, ({ date }) => date);
+const result = examlist
+   .sort((a, b) => {
+      /** Sorts schedules by date and time created
+       * With earliest showing first*/
+      return Date.parse(a.date) - Date.parse(b.date);
+   })
+   //reducing to sort by date value as a key
+   .reduce((r, a) => {
+      r[a.date] = r[a.date] || [];
+      r[a.date].push(a);
+      return r;
+   }, Object.create(null));
 
 export default result;
