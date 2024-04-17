10d845e1b2c98b1f0e01ae503c01a41c297120e5
Akeem Smith <asmith.339@outlook.com>
2024-04-17 08:29:25 -0400

    Feteching data from supabase instead of json file

--- frontend/src/components/table.tsx
+++ frontend/src/components/table.tsx
@@ -1,11 +1,12 @@
 //DaisyUI table
-import result from "../data/examlist";
 import Divider from "./ui/divider";
 import Searchbar from "./ui/searchbar";
-import { Fragment, useState } from "react";
+import { Fragment, useEffect, useState } from "react";
 import React from "react";
+import supabase from "../utils/supabaseClient";
 
 type Exam = {
+   date: string | any;
    code: string;
    title: string;
    time: string;
@@ -14,102 +15,140 @@ type Exam = {
    room: string;
 };
 const Table = () => {
+   const [hasLoaded, setHasLoaded] = useState(false);
+   const [loadingError, setLoadingError] = useState(false);
    const [search, setSearch] = useState("");
+   const [result, setResult] = useState<any>([]);
 
    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
       setSearch(event.target.value);
    };
 
-   //Setting table data
-   const tabledata = (
-      <>
-         {Object.keys(result).map((date) => {
-            {
-               /**Variable filterResults stores returned exam info corresponding to user input into an array */
-            }
-            let filterResults = result[date].filter((exam: Exam) => {
-               return search.toLowerCase() === ""
-                  ? exam
-                  : exam.title.toLowerCase().includes(search.toLowerCase()) ||
-                       exam.code.toLowerCase().includes(search.toLowerCase());
-            });
-            return (
-               <Fragment key={date}>
-                  {filterResults.length > 0 && (
-                     <>
-                        <p
-                           className={
-                              "dark:text-white font-semibold text-base md:text-lg sticky -top-4 bg-slate-50 py-2 dark:bg-gray-900 z-10"
-                           }
-                        >
-                           {date}
-                        </p>
-                        <div className="overflow-x-auto">
-                           <table
-                              key={date}
-                              className="table table-sm 
-       md:table-md dark:text-white text-black"
-                           >
-                              <thead>
-                                 <tr className="bg-slate-100 dark:bg-gray-800 text-xs text-gray-600 md:text-base dark:text-white">
-                                    <td>Course Code</td>
-                                    <td>Course Name</td>
-                                    <td>Time</td>
-                                    <td>Length</td>
-                                    <td>Location</td>
-                                    <td>Room</td>
-                                 </tr>
-                              </thead>
-                              <tbody>
-                                 {filterResults.map(
-                                    (exam: Exam, index: number) => {
-                                       return (
-                                          <tr key={index}>
-                                             <td key={exam.code}>
-                                                {exam.code}
-                                             </td>
-                                             <td key={exam.title}>
-                                                {exam.title}
-                                             </td>
-                                             <td key={exam.time}>
-                                                {exam.time}
-                                             </td>
-                                             <td key={exam.hours}>
-                                                {exam.hours}
-                                             </td>
-                                             <td key={exam.location}>
-                                                {exam.location}
-                                             </td>
-                                             <td key={exam.room}>
-                                                {exam.room}
-                                             </td>
-                                          </tr>
-                                       );
-                                    }
-                                 )}
-                              </tbody>
-                           </table>
-                        </div>
-                        <Divider />
-                     </>
-                  )}
-               </Fragment>
-            );
-         })}
-      </>
-   );
+   /**Get Table data from Supabase, store in result state variable
+    * and set hasLoaded to true, displaying the table
+    */
+   const getTableData = async () => {
+      let { data: exams, error } = await supabase.from("exams").select("*");
+
+      if (exams) {
+         let examListGrouped = exams.reduce(
+            (result: any, currentValue: any) => {
+               (result[currentValue["date"]] =
+                  result[currentValue["date"]] || []).push(currentValue);
+               return result;
+            },
+            {}
+         );
+         setResult(examListGrouped);
+         setHasLoaded(true);
+      }
+
+      if (error) {
+         setLoadingError(true);
+      }
+   };
+
+   //load exams from supabase DB every time hasLoaded changes
+   useEffect(() => {
+      getTableData();
+   }, [hasLoaded]);
 
    //Table data returned
    return (
       <>
+         {loadingError && (
+            <div className="toast z-20">
+               <div className="alert alert-error">
+                  <span>The exam list could not be loaded.</span>
+               </div>
+            </div>
+         )}
+
          <div className="w-100 mb-8">
             <Searchbar
                placeholder="Search by code name or code"
                onChange={handleChange}
                value={search}
+               disabled={!hasLoaded}
             />
          </div>
-         {tabledata}
+         {hasLoaded === false ? (
+            <div className="flex flex-col items-center mb-10">
+               <p className="mb-3 font-semibold text-lg">
+                  Loading exam list...
+               </p>
+               <span className="loading loading-dots loading-lg"></span>
+            </div>
+         ) : (
+            <>
+               {" "}
+               {Object.keys(result).map((date) => {
+                  {
+                     /**Variable filterResults stores returned exam info corresponding to user input into an array */
+                  }
+                  let filterResults = result[date].filter((exam: Exam) => {
+                     return search.toLowerCase() === ""
+                        ? exam
+                        : exam.title
+                             .toLowerCase()
+                             .includes(search.toLowerCase()) ||
+                             exam.code
+                                .toLowerCase()
+                                .includes(search.toLowerCase());
+                  });
+                  return (
+                     <Fragment key={date}>
+                        {filterResults.length > 0 && (
+                           <>
+                              <p
+                                 className={
+                                    "dark:text-white font-semibold text-base md:text-lg sticky -top-4 bg-slate-50 py-2 dark:bg-gray-900 z-10"
+                                 }
+                              >
+                                 {date}
+                              </p>
+                              <div className="overflow-x-auto">
+                                 <table
+                                    key={date}
+                                    className="table table-sm 
+          md:table-md dark:text-white text-black"
+                                 >
+                                    <thead>
+                                       <tr className="bg-slate-100 dark:bg-gray-800 text-xs text-gray-600 md:text-base dark:text-white">
+                                          <td>Course Code</td>
+                                          <td>Course Name</td>
+                                          <td>Time</td>
+                                          <td>Length</td>
+                                          <td>Location</td>
+                                          <td>Room</td>
+                                       </tr>
+                                    </thead>
+                                    <tbody>
+                                       {filterResults.map(
+                                          (exam: Exam, index: number) => {
+                                             return (
+                                                <tr key={index}>
+                                                   <td>{exam.code}</td>
+                                                   <td>{exam.title}</td>
+                                                   <td>{exam.time}</td>
+                                                   <td>{exam.hours}</td>
+                                                   <td>{exam.location}</td>
+                                                   <td>{exam.room}</td>
+                                                </tr>
+                                             );
+                                          }
+                                       )}
+                                    </tbody>
+                                 </table>
+                              </div>
+                              <Divider />
+                           </>
+                        )}
+                     </Fragment>
+                  );
+               })}
+            </>
+         )}
       </>
    );
 };
--- frontend/src/components/ui/searchbar.tsx
+++ frontend/src/components/ui/searchbar.tsx
@@ -2,15 +2,17 @@ type Props = {
    placeholder: string;
    value?: string;
    onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
+   disabled: boolean;
 };
 
-const Searchbar = ({ placeholder, onChange }: Props) => {
+const Searchbar = ({ placeholder, onChange, disabled }: Props) => {
    return (
       <input
          type="text"
          className="w-full bg-slate-100 text-black p-4 border border-slate-400 outline-none rounded-lg placeholder-gray-500 focus:ring-1 ring-gray-400 dark:bg-gray-800 dark:text-white dark:placeholder-gray-300 dark:focus:ring-gray-300"
          placeholder={placeholder}
          onChange={onChange}
+         disabled={disabled}
       />
    );
 };
